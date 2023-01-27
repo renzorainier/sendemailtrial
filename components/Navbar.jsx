790e5e06b616217fd5e38d6169909cf133dc5526
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-27 04:42:38 +0000

    Fri, Jan 27, 2023, 4:42 AM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="" />
+      </div>
+    </div>
   )
 }
 
