8b8dc4c2fc357df199b69925f9aab4e4eea1c459
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-27 08:11:42 +0000

    Fri, Jan 27, 2023, 8:11 AM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import { BsFillPersonLinesFill } from "react-icons/bs"
 
 const Navbar = () => {
 
-  const[nav, setNav] = usedState(false)
+  const[nav, setNav] = useState(false)
 
   const handleNav = () => {
     setNav(true)
@@ -47,8 +47,11 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/70" >
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+      <div className={nav ? "fixed left-0 top-0 w-full h-screen bg-black/70" : ""} >
+        <div className={
+        nav
+        ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+        : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"}>
         <div>
           <div className="flex w-full items-center justify-between">
             <Image
