c37ad983e11cbb718e3d1dc0c3d695f97e441a51
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-27 06:11:19 +0000

    Fri, Jan 27, 2023, 6:11 AM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -55,6 +55,26 @@ function Navbar() {
             <p className="w-[85%] md:w-[90%] py-4">Let's build something legendary together</p>
           </div>
         </div>
+        <div>
+          <ul className="uppercase">
+            <Link href="/">
+              <li className="py-4 text-sm">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="py-4 text-sm">About</li>
+            </Link>
+            <Link href="/">
+              <li className="py-4 text-sm">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="py-4 text-sm">Projects</li>
+            </Link>
+            <Link href="/">
+              <li className="py-4 text-sm">Contact</li>
+            </Link>
+
+          </ul>
+        </div>
         </div>
       </div>
     </div>
