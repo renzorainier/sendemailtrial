c0140e60d60dceb7d6c80d118df0d1cd81cde1c7
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-27 04:45:22 +0000

    Fri, Jan 27, 2023, 4:45 AM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image src="" />
+        <Image src="https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png" />
       </div>
     </div>
   )
