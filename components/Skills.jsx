bdd4c2cd6729c3413fea6fde657aa0be94a17d7d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 14:49:33 +0000

    Sat, Jan 28, 2023, 2:49 PM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex-col justify-center h-full">
+        
+      </div>
+    </div>
   )
 }
 
