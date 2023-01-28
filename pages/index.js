4512951b77f62899754441c977f5780e2e47e8fb
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 14:35:14 +0000

    Sat, Jan 28, 2023, 2:35 PM +00:00

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Navbar from 'components/Navbar'
 import Main from "components/Main"
 import About from "components/About"
+import Skills from "components/Skills"
 
 
 export default function Home() {
