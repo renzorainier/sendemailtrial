82dd0e3ff81c1ebcd8032224ef258d1811b1104e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 10:49:05 +0000

    Sat, Jan 28, 2023, 10:49 AM +00:00

---
+++ components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className="w-full h-screen text-center">
+            <div className="max-w-[1240px] w-full mx-auto p-2 flex justify-center items-center">
+                <div>
+                    <p className="uppercase text-sm tracking-widest text-gray-600"> LETS BUILD SOMETHING LEGENDARY TOGETHER</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
