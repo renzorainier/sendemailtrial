639d08b225b14b982a375fa49ae813fff3f7849b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 14:31:15 +0000

    Sat, Jan 28, 2023, 2:31 PM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white">
+    <div className="fixed w-full h-20 shadow-xl z-[100] bg-violet-400">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image
           src="/../public/assets/netflix.png"
