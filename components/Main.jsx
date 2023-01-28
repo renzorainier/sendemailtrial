959d80a8f5fec48f1ed89f54e37ab33cab44e19e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 10:57:22 +0000

    Sat, Jan 28, 2023, 10:57 AM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 function Main() {
     return (
         <div className="w-full h-screen text-center">
-            <div className="max-w-[1240px] w-full mx-auto p-2 flex justify-center items-center">
+            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
                 <div>
                     <p className="uppercase text-sm tracking-widest text-gray-600"> LETS BUILD SOMETHING LEGENDARY TOGETHER</p>
                     <h1 className="py-4 text-gray-700">Hi, I'am <span className="text-[#5651e5]">Renz</span></h1>
-                    <h1 className="py-4 text-gray-700">
+                    <h1 className="py-2 text-gray-700">
                         A Front-End WannaBe
                     </h1>
+                    <p className="py-2 text-gray-600 max-w-[70%] m-auto">
+
+                    </p>
 
                 </div>
             </div>
