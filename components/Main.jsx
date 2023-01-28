7a19097d6fa36d28db805ba61687960bc54eaf34
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 11:02:28 +0000

    Sat, Jan 28, 2023, 11:02 AM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,11 +8,16 @@ function Main() {
                     <p className="uppercase text-sm tracking-widest text-gray-600"> LETS BUILD SOMETHING LEGENDARY TOGETHER</p>
                     <h1 className="py-4 text-gray-700">Hi, I'am <span className="text-[#5651e5]">Renz</span></h1>
                     <h1 className="py-2 text-gray-700">
-                        A Front-End WannaBe
+                        A Front-End Dev Wanna Be
                     </h1>
                     <p className="py-2 text-gray-600 max-w-[70%] m-auto">
-
+                        Just a simple guy who is addicted to coding
                     </p>
+                    <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                        <div className="">
+                            <FaFacebook />
+                        </div>
+                    </div>
 
                 </div>
             </div>
