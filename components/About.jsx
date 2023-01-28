350aa2c774c88929b65bb672aa7d5a744290a484
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 13:53:53 +0000

    Sat, Jan 28, 2023, 1:53 PM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -11,6 +11,7 @@ function About() {
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam facilisis, orci sit amet rhoncus ornare, sem lacus placerat erat, in blandit lacus est a diam. Fusce arcu eros, congue ac lorem sit amet, iaculis molestie odio. Vivamus scelerisque risus a ante facilisis malesuada. Mauris sed orci at erat sodales sollicitudin eget nec purus. In hac habitasse platea dictumst. Sed ut cursus nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur non elit at velit faucibus lacinia. Aliquam vel erat urna.
                     </p>
+                    <p>Check out some of my latest projects</p>
                 </div>
                 <div></div>
             </div>
