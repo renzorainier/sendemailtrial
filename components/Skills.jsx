7742e30afa431d77711f8622ebb044e829ce145a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 14:51:16 +0000

    Sat, Jan 28, 2023, 2:51 PM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Skills() {
+const Skills = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex-col justify-center h-full">
-        
+        <p>Skills</p>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
       <Navbar/>
       <Main />
       <About />
+      <Skills />
     </div>
   )
 }
