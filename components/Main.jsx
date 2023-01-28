262c284602094e835b8ff3d03faaefec6c83400b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 10:55:19 +0000

    Sat, Jan 28, 2023, 10:55 AM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,6 +6,11 @@ function Main() {
             <div className="max-w-[1240px] w-full mx-auto p-2 flex justify-center items-center">
                 <div>
                     <p className="uppercase text-sm tracking-widest text-gray-600"> LETS BUILD SOMETHING LEGENDARY TOGETHER</p>
+                    <h1 className="py-4 text-gray-700">Hi, I'am <span className="text-[#5651e5]">Renz</span></h1>
+                    <h1 className="py-4 text-gray-700">
+                        A Front-End WannaBe
+                    </h1>
+
                 </div>
             </div>
         </div>
