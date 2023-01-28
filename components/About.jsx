a87a3eba3ee95fa16d3869f4f682a8eac414cdaa
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-28 11:16:23 +0000

    Sat, Jan 28, 2023, 11:16 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function About() {
     return (
-        <div>About</div>
+        <div className="w-full md:h-screen p-2 flex items-center py-16">
+            <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+                <div className="col-span-2">
+                    <p>About</p>
+                    <h2>Who I am</h2>
+                    <p>//I am a beginner developer</p>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
