6d6f7aaec41404c334b4a868e4361e643329053a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-29 15:31:36 +0000

    Sun, Jan 29, 2023, 3:31 PM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,7 +5,10 @@ const Skills = () => {
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex-col justify-center h-full">
         <p className="text-xl tracking-widest uppercase text">Skills</p>
-        
+        <h2 className="py-4">What can I do</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+
+        </div>
       </div>
     </div>
   )
