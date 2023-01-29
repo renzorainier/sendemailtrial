874b783a1c297df1d534ec8a9909082848339e61
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-29 15:24:04 +0000

    Sun, Jan 29, 2023, 3:24 PM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -4,8 +4,8 @@ const Skills = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex-col justify-center h-full">
-        <p>Skills</p>
-        
+        <p className="text-xl tracking-widest uppercase">Skills</p>
+
       </div>
     </div>
   )
