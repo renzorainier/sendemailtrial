1669178f375d7b8c8b0603026733cc123e7c7ac1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-29 15:42:33 +0000

    Sun, Jan 29, 2023, 3:42 PM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Skills = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">Skills</p>
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
         <h2 className="py-4">What can I do</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-
-
-        <div className="">
-
-        </div>
-
-
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/html.png"
+                  width="64"
+                  heigth="64px"
+                  alt="/"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
