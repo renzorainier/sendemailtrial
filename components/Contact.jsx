8cfaeb77be4f187b5da44bb77cbacad38de02d0a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 15:33:07 +0000

    Wed, Feb 1, 2023, 3:33 PM +00:00

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaFacebook, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
 
 function Contact() {
   return (
@@ -9,26 +13,52 @@ function Contact() {
         </p>
         <h2 className="py-4">Let's get in touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
-
           {/* left */}
-        <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="lg:p-4 h-full">
-            <div>
-              <img className="rounded-xl hover:scale-105 ease-in duration-300" src="https://images.unsplash.com/photo-1612296727716-d6c69d2a2cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="" />
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+            <div className="lg:p-4 h-full">
+              <div>
+                <img
+                  className="rounded-xl hover:scale-105 ease-in duration-300"
+                  src="https://images.unsplash.com/photo-1612296727716-d6c69d2a2cbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h2 className="py-2">Renz Rainier Pasagdan</h2>
+                <p>Front-End Wanna Be</p>
+                <p className="py-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam nulla erat, accumsan dapibus fringilla vel, iaculis id
+                  purus. Fusce accumsan quam sed mollis varius. Aliquam
+                  dignissim sollicitudin orci, ut commodo turpis porttitor quis.
+                  Etiam non risus eget ante suscipit efficitur in quis diam.
+                  Nullam dignissim non enim sed scelerisque. Sed posuere in
+                  justo sit amet luctus.
+                </p>
+              </div>
             </div>
             <div>
-              <h2 className="py-2">Renz Rainier Pasagdan</h2>
-              <p>Front-End Wanna Be</p>
-              <p className="py-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nulla erat, accumsan dapibus fringilla vel, iaculis id purus. Fusce accumsan quam sed mollis varius. Aliquam dignissim sollicitudin orci, ut commodo turpis porttitor quis. Etiam non risus eget ante suscipit efficitur in quis diam. Nullam dignissim non enim sed scelerisque. Sed posuere in justo sit amet luctus.</p>
+              <p className="uppercase pt-8">Connect with me</p>
+              <div className="flex items-center justify-betweeen py-4">
+                <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+                    <FaFacebook />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+                    <FaGithub />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+                    <AiOutlineMail />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div>
-            <p className="uppercase pt-8">Connect with me</p>
-          </div>
-        </div>
 
           {/* right */}
-
         </div>
       </div>
     </div>
