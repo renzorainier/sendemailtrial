0f722539d94cb0b7d6fbf47ecf5b49d97f66cdfd
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 11:02:50 +0000

    Wed, Feb 1, 2023, 11:02 AM +00:00

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -15,10 +15,10 @@ const Projects = () => {
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
             <Image className="rounded-xl group-hover:opacity-10" src={propertyImg} alt="/" />
             <div className="hidden group-hover:block absolute top-[50%] translate-x-[-50%] translate-y-[-50%]">
-              <h3 className="">Property Finder</h3>
-              <p>React JS</p>
+              <h3 className="text-2xl text-white tracking-wider text-center">Property Finder</h3>
+              <p className="pb-4 pt-2 text-white text-center">React JS</p>
               <Link href="/">
-                <p>More Info</p>
+                <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">More Info</p>
               </Link>
             </div>
           </div>
