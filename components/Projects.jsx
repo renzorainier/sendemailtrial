6ba8e6168ccc5bf6df88cea2e94a3c864a1b7edb
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 16:31:51 +0000

    Wed, Feb 1, 2023, 4:31 PM +00:00

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,7 +9,7 @@ import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="p-4 text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
