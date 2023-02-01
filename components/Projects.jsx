b8e4683a0daf4405f86021b8a1f7b52b5ffc160a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 10:42:52 +0000

    Wed, Feb 1, 2023, 10:42 AM +00:00

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image"
+import propertyImg from ".."
 
 function Projects() {
   return (
@@ -10,6 +12,7 @@ function Projects() {
         <h2 className="p-4">What I've Build</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]"></div>
+          <Image src={propertyImg}/>
         </div>
       </div>
     </div>
