5da22727da64bf8f91587e48fdaeca88b6a4aa5d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 16:53:57 +0000

    Wed, Feb 1, 2023, 4:53 PM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,11 +33,11 @@ const Navbar = () => {
       }
     >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image
-          src="/../public/assets/netflix.png"
+        <Image className="rounded-full"
+          src="/../public/assets/me.jpg"
           alt="/"
-          width="100"
-          height="100"
+          width="75"
+          height="75"
         />
         <div>
           <ul className="hidden md:flex">
