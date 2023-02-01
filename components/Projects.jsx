64e1aff494e93972a3900eee4bb8e1af31ff611c
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-01 10:37:43 +0000

    Wed, Feb 1, 2023, 10:37 AM +00:00

---
+++ components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="w-ful">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widestuppercase text-[#5651e5]"></p>
+        <h2 className="p-4">What I've Build</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
