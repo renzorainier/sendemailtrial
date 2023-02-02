2eb198c7bede45ad32f6e508f7bb276957b9f330
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 15:32:13 +0000

    Thu, Feb 2, 2023, 3:32 PM +00:00

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-[#e8c284] rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
         className="rounded-xl group-hover:opacity-10"
         src={backgroundImg}
