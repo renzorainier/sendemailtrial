6957006214ad490ef1b33795b13bec491b4598cb
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 07:41:22 +0000

    Thu, Feb 2, 2023, 7:41 AM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       }
     >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <img className="h-20" src="https://cdn-icons-png.flaticon.com/512/9168/9168209.png" alt="" />
+        <img className="h-10" src="https://cdn-icons-png.flaticon.com/512/9168/9168209.png" alt="" />
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
