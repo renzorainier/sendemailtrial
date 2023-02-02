81ef09045f7e2920d9eb1e6f8d6163dcff076ead
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 08:15:16 +0000

    Thu, Feb 2, 2023, 8:15 AM +00:00

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,8 +5,6 @@ import propertyImg from "../public/assets/projects/property.jpg";
 import cryptoImg from "../public/assets/projects/crypto.jpg";
 import netflixImg from "../public/assets/projects/netflix.jpg";
 import twitchImg from "../public/assets/projects/twitch.jpg";
-import pythonImg from "../public/assets/projects/python.jpg";
-import cImg from "../public/assets/projects/c.jpg";
 
 const Projects = () => {
   return (
