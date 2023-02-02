49f9f08b96369e185c055513da9fc86795a85010
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 08:40:21 +0000

    Thu, Feb 2, 2023, 8:40 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div id="about"className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e1]">About</p>
+          <p className="uppercase text-xl tracking-widest text-[#C58940]">About</p>
           <h2 className="py-4">Who I am</h2>
           <p className="py-2 text-gray-600">//I am a beginner developer</p>
           <p className="py-2 text-gray-600">
