2fc2c75e82c20506a86fb9f24a15c792302ac1a2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 08:23:39 +0000

    Thu, Feb 2, 2023, 8:23 AM +00:00

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -76,6 +76,21 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="/../public/skills/c.png"
+                  width="64"
+                  height="64"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justfify-center">
+                <h3>C</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src="/../public/skills/react.png"
                   width="64"
                   height="64"
