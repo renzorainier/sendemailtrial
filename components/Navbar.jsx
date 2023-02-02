a129a747e5ea4ba5595c2d19716ae2e0f19fb8d4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-02 15:18:59 +0000

    Thu, Feb 2, 2023, 3:18 PM +00:00

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className="py-4">
-              <p className="uppercase tracking-widest text-[#e8c284]">
+              <p className="uppercase tracking-widest text-[#C58940]">
                 Let's Connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
